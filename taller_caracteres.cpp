33e96bd71f964d1b82e8f5133ad9f996e613bbdf
Cesar Osorio <cesarozorioa@hotmail.com>
2022-08-08 09:27:58 -0500

    3er commit Definicion de variable locales en las funciones

--- taller_caracteres.cpp
+++ taller_caracteres.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 void llenaLetras(char v[])
 {
+    int i, j;
 }
 void ordenaLetras(char v[])
 {
+    int i, j;
 }
-void buscarLetra(char v[])
-{
-}
-main()
+void buscarLetra(char v[]){
+    int i, j} main()
 {
     char letras[100];
     llenaLetras(letras);
