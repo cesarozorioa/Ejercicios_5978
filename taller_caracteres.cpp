08f32366d938ff4492d3026d1a11c08f98ed6fbf
Cesar Osorio <cesarozorioa@hotmail.com>
2022-08-09 15:37:44 -0500

    5to commit v1 funcion llenaLetras

--- taller_caracteres.cpp
+++ taller_caracteres.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 void llenaLetras(char v[])
 {
-    int i, j;
+    int i, j, num, jx;
+    int lc, i;
+    char aux[10];
+    srand(time(NULL));
+    lc = 1 + rand() % 10;
+    for (int j = 0; j < 10; j++)
+    {
+        aux[j] = 97 + rand() % 26;
+    }
+
+    for (i = 0, jx = 0; i < lc; i++)
+    {
+        if (v[i] != aux[jx])
+        {
+            v[i] = aux[jx];
+        }
+        else
+        {
+            jx++;
+        }
+    }
+    v[i] = '\0';
 }
 void ordenaLetras(char v[])
 {
     int i, j;
 }
-void buscarLetra(char v[]){
+void buscarLetra(char v[])
+{
     int i, j;
-} 
+}
 main()
 {
     char letras[100];
